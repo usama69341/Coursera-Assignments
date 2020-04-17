8bb07ca7408b68cf947a7ba7b7f72dbf8e2568cb
dolathamza <59790244+dolathamza@users.noreply.github.com>
2020-04-17 23:35:53 +0500

    Module 4 Solution

---
+++ module4-solution/script.js
@@ -0,0 +1,11 @@
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var name of names) {
+        var firstLetter = name.charAt(0).toLowerCase();
+        if (firstLetter == "j") {
+            byeSpeaker.speak(name);
+        } else {
+            helloSpeaker.speak(name);
+        }
+    }
+})();
